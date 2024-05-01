ecfabfcee477c8b0629a3226d3ee8ab88c22dc2c
henriquekutnercordeiro <hkutner@ciandt.com>
2024-05-01 08:01:23 -0300

    commentary

--- A_arrays/reversedStringHard.js
+++ A_arrays/reversedStringHard.js
@@ -1,5 +1,5 @@
 // Time complexity O(n)
-// Reverse an array of strings but do not create a new array.
+// Reverse an array of strings but do not create a new array. (Ignoring the fact that strings in javascript are immutable)
 function reverseStringHard(value) {
   let sizeValue = value.length - 1;
   for (let i = 0; i < sizeValue; i++) {

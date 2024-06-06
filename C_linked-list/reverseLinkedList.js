eddbef4f1e2ad70df35992b6c81e3638a7e4d11e
henriquekutnercordeiro <hkutner@ciandt.com>
2024-06-06 12:58:13 -0300

    reverse linked list

---
+++ C_linked-list/reverseLinkedList.js
@@ -0,0 +1,98 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this.printList();
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.tail = this.tail;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  remove(index) {
+    if (index > this.length) {
+      return console.log("This index does not exist.");
+    }
+    const nodeToRemove = this.traverseToIndex(index);
+    const leader = this.traverseToIndex(index - 1);
+    leader.next = nodeToRemove.next;
+    this.length++;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  reverse() {
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this.printList();
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return console.log(array);
+  }
+}
+
+const myLinkedList = new LinkedList(0);
+
+myLinkedList.append(5);
+myLinkedList.append(10);
+myLinkedList.append(15);
+myLinkedList.reverse();
